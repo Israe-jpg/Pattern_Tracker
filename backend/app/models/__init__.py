eb4a5688c4f68daab6e2ab94668b0f4c634da511
Israe-jpg <israeguennouni6@gmail.com>
2025-10-04 14:54:40 +0100

    migrate the new tracker_field table and import it in innit

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,5 +2,6 @@ from .user import User
 from .tracker_category import TrackerCategory
 from .tracker import Tracker
 from .tracking_data import TrackingData
+from .tracker_field import TrackerField
 
-__all__ = ['User', 'TrackerCategory', 'Tracker', 'TrackingData']
+__all__ = ['User', 'TrackerCategory', 'Tracker', 'TrackingData', 'TrackerField']
