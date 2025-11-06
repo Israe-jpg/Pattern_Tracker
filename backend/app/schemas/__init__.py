abd362f82cec989fa2d06ead5bc9725f4cc4c097
Israe-jpg <israeguennouni6@gmail.com>
2025-11-06 10:12:50 +0100

    fix spelling bug

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,5 +1,5 @@
 from .user_schemas import UserRegistrationSchema
 from .tracker_schemas import TrackerSchema
-from .tracking_data_schemas import TrackingDataSchema
+from .tracking_data_schema import TrackingDataSchema
 
 __all__ = ['UserRegistrationSchema', 'TrackerSchema', 'TrackingDataSchema']
