11d56ff22980f1d9a71bb7fd0903c5ec660c6cb2
Israe-jpg <israeguennouni6@gmail.com>
2026-01-02 18:53:14 +0100

    Create trackerService.js

---
+++ frontend/src/services/trackerService.js
@@ -0,0 +1,23 @@
+import api from './api';
+import { API_ENDPOINTS } from '../constants/config';
+
+export const trackerService = {
+  // Get all user's trackers
+  getMyTrackers: async () => {
+    const response = await api.get(API_ENDPOINTS.MY_TRACKERS);
+    return response.data;
+  },
+
+  // Get tracker details
+  getTrackerDetails: async (trackerId) => {
+    const response = await api.get(API_ENDPOINTS.TRACKER_DETAILS(trackerId));
+    return response.data;
+  },
+
+  // Get form schema for a tracker
+  getFormSchema: async (trackerId) => {
+    const response = await api.get(API_ENDPOINTS.FORM_SCHEMA(trackerId));
+    return response.data;
+  },
+};
+
